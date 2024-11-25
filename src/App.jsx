e0ed17e8d9e97c5b29c7b8f90a7a54182aa48455
Aliva Nurramadhan <86903281+AlipTamvan@users.noreply.github.com>
2024-11-26 00:44:48 +0800

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+
+
+function App() {
+  return <h1 className="text-5xl">Hello World</h1>;
+}
+
+export default App;
+
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@routes": path.resolve(__dirname, "src/routes"),
+      "@service": path.resolve(__dirname, "src/service"),
+      "@store": path.resolve(__dirname, "src/store"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
+});
